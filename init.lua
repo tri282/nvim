cffafcc940777c628e63c6b3782643307753e338
tri282 <minhtri.do2802@gmail.com>
2025-06-30 21:56:13 -0400

    [ADD] formatter, autocompletion

--- init.lua
+++ init.lua
@@ -10,30 +10,30 @@
 --- lazy is a plugins/package manager for Neovim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
 --- Lazy setup
-require('lazy').setup {
- require 'plugins.neotree',
- require 'plugins.catppuccin',
- require 'plugins.lualine',
- require 'plugins.treesitter',
- require 'plugins.telescope',
- require 'plugins.lsp',
-}
+require("lazy").setup({
+	require("plugins.neotree"),
+	require("plugins.catppuccin"),
+	require("plugins.lualine"),
+	require("plugins.treesitter"),
+	require("plugins.telescope"),
+	require("plugins.lsp"),
+	require("plugins.conform"),
+	require("plugins.blink_cmp"),
+})
 
 require("vim-options")
-
-
---
+++ lua/plugins/conform.lua
@@ -0,0 +1,44 @@
+--- CONFORM
+--- formatter
+
+return {
+  'stevearc/conform.nvim',
+  event = { 'BufWritePre' },
+  cmd = { 'ConformInfo' },
+  keys = {
+    {
+      '<leader>f',
+      function()
+        require('conform').format { async = true, lsp_format = 'fallback' }
+      end,
+      mode = '',
+      desc = '[F]ormat buffer',
+    },
+  },
+  opts = {
+    notify_on_error = false,
+    format_on_save = function(bufnr)
+      local disable_filetypes = { c = true, cpp = true }
+      if disable_filetypes[vim.bo[bufnr].filetype] then
+        return nil
+      else
+        return {
+          timeout_ms = 500,
+          lsp_format = 'fallback',
+        }
+      end
+    end,
+    formatters_by_ft = {
+      lua = { "stylua" },
+      c = { "astyle", "clang-format", "uncrustify" },
+      html = { "djlint", "html_beautify" },
+      xml = { "xmllint", "xmlformatter" },
+      -- Conform will run multiple formatters sequentially
+      python = { "isort", "black" },
+      -- You can customize some of the format options for the filetype (:help conform.format)
+      rust = { "rustfmt", lsp_format = "fallback" },
+      -- Conform will run the first available formatter
+      javascript = { "prettierd", "prettier", stop_after_first = true },
+    },
+  },
+}
