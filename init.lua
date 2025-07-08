6f90a624c67dc15fb72ec2f5958cb5c69d364ad1
tri282 <minhtri.do2802@gmail.com>
2025-07-08 17:34:12 -0400

    [ADD] latest

--- init.lua
+++ init.lua
@@ -4,6 +4,9 @@
 --- ctrl + f + g: live grep + jump
 --- ctrl + n: show file system, default: on left
 
+--- ctrl + d + t: breakpoint for debugging
+--- ctrl + d + c: continue debugging
+
 --- SOME KEYBINDS END
 
 --- LAZY
@@ -24,7 +27,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
---- Lazy setup
+--- lazy setup
 require("lazy").setup({
 	require("plugins.neotree"),
 	require("plugins.catppuccin"),
@@ -35,6 +38,64 @@ require("lazy").setup({
 	require("plugins.conform"),
 	require("plugins.blink_cmp"),
 	require("plugins.debugging"),
+	require("plugins.gitsigns"),
+	require("plugins.comments"),
 })
 
 require("vim-options")
+
+--- FORMATTING
+
+--- default set numbers
+vim.o.number = true
+
+--- relative line numbers
+vim.o.relativenumber = true
+
+--- enable mouse mode
+vim.o.mouse = "a"
+
+--- hide mode since already configured the status
+vim.o.showmode = false
+
+--- sync nvim and OS's clipboard
+vim.schedule(function()
+	vim.o.clipboard = "unnamedplus"
+end)
+
+--- break indent
+vim.o.breakindent = true
+
+--- case sensitive searching UNLESS \C or one or more capital letters in the search term
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+--- configure how new splits should be opened
+vim.o.splitright = true
+vim.o.splitbelow = true
+
+--- set whitespace chars
+vim.o.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+--- show line number that cursor is on
+vim.o.cursorline = true
+
+--- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+--- instead raise a dialog asking if you wish to save the current file(s)
+vim.o.confirm = true
+
+--- highlight when yanking texts
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.hl.on_yank()
+	end,
+})
+
+--- switch between windows
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,12 +1,67 @@
 --- CATPPUCCIN
 --- themes
 
+
 return {
   "catppuccin/nvim",
   lazy = false,
   name = "catppuccin",
   priority = 1000,
+
   config = function()
-    vim.cmd.colorscheme "catppuccin"
-  end
+    local latte = require("catppuccin.palettes").get_palette "latte"
+    local frappe = require("catppuccin.palettes").get_palette "frappe"
+    local macchiato = require("catppuccin.palettes").get_palette "macchiato"
+    local mocha = require("catppuccin.palettes").get_palette "mocha"
+
+    require("catppuccin").setup({
+      flavour = "mocha", -- latte, frappe, macchiato, mocha
+      background = {
+        light = "latte",
+        dark = "mocha",
+      },
+      transparent_background = false,
+      show_end_of_buffer = false,
+      term_colors = false,
+      dim_inactive = {
+        enabled = true,
+        shade = "dark",
+        percentage = 0.15,
+      },
+      no_italic = false,
+      no_bold = false,
+      no_underline = false,
+      styles = {
+        comments = { "italic" },
+        conditionals = { "italic" },
+        loops = {},
+        functions = {},
+        keywords = {},
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = {},
+        properties = {},
+        types = {},
+        operators = {},
+      },
+      color_overrides = {},
+      custom_highlights = {},
+      default_integrations = true,
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+        notify = false,
+        mini = {
+          enabled = true,
+          indentscope_color = "",
+        },
+      },
+    })
+
+    -- setup must be called before loading
+    vim.cmd.colorscheme("catppuccin")
+  end,
 }
